3b369ad7f2aefe7c4793f0a44f327e2820798107
Jessibel Virtucio <jessievirtucio@gmail.com>
2025-01-14 07:26:31 +0000

    Add HTML to Jest mock DOM testing p1

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,13 @@
+const buttonClick = require("./button");
+
+
+beforeEach( () => {
+    document.body.innerHTML = "<p id='par'></p>";
+})
+
+describe("DOM test", () => {
+    test("expect p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked!");
+    })
+})
